c8d5c8c80ea5f8a6228daae26e217b417a5b3ed5
William Quintal <william95quintalwilliam@outlook.com>
2019-07-16 10:08:34 -0400

    prout

--- src/app/fileexplorer/fileexplorer.component.ts
+++ src/app/fileexplorer/fileexplorer.component.ts
@@ -4,7 +4,7 @@ import { FileExplorerRPClient, FileInfo } from './fe-rpc';
 @Component({
   selector: 'app-file-explorer',
   template: `
-    <app-navigation-bar [path]="path" [files]="files" (onPrevious)="goBack()"></app-navigation-bar>
+    <app-navigation-bar [path]="path" [files]="files" (onPrevious)="goBackHistory()"></app-navigation-bar>
 
     <app-file-table [path]="path" [files]="files" (onNavigate)="cd($event)"></app-file-table>
   `
@@ -12,6 +12,8 @@ import { FileExplorerRPClient, FileInfo } from './fe-rpc';
 export class FileExplorerComponent implements OnInit {
   files: FileInfo[];
   path: string = '/';
+
+  previous_dir: string[] = [];
 
   constructor(private client: FileExplorerRPClient) {}
 
@@ -21,8 +23,18 @@ export class FileExplorerComponent implements OnInit {
 
   cd(info: FileInfo) {
     if (info.isDir) {
+      this.previous_dir.push(this.path);
       this.path = info.path + (info.path.endsWith('/') ? '' : '/') + info.name;
       this.client.Ls(this.path).subscribe(f => this.subLs(f));
+    }
+  }
+
+  goBackHistory() {
+    if (this.previous_dir.length > 0) {
+      const previous = this.previous_dir.pop();
+      this.path = previous;
+      this.client.Ls(this.path).subscribe(f => this.subLs(f));
+      console.log(previous);
     }
   }
 
